88392064eee110800c449172d4cb45996026a9da
Igor Sinyak <igorsinyak@MacBook-Pro-Igor.local>
2023-05-23 15:35:38 +0300

    feat: add base href

--- scully.product-documentation.config.ts
+++ scully.product-documentation.config.ts
@@ -1,18 +1,41 @@
-import { RouteConfig, ScullyConfig, setPluginConfig } from '@scullyio/scully';
+import { baseHrefRewrite } from '@scullyio/scully-plugin-base-href-rewrite';
+import { ScullyConfig, setPluginConfig } from '@scullyio/scully';
 
 /** this loads the default render plugin, remove when switching to something else. */
 import '@scullyio/scully-plugin-puppeteer';
 import { copyToClipboardPlugin } from './scully/plugins/copyToClipboard';
 import { replaceImgSrc } from './scully/plugins/replaceImgSrc';
+import { moveContentFiles } from './scully/plugins/move-content-files/move-content-files';
 import 'prismjs/components/prism-ruby.min.js';
+
+const baseHref = 'docs';
 
 const defaultPostRenderers = [copyToClipboardPlugin, replaceImgSrc];
 setPluginConfig('md', { enableSyntaxHighlighting: true });
+setPluginConfig(baseHrefRewrite, { href: baseHref });
+setPluginConfig(moveContentFiles, {
+    root: baseHref,
+    categories: [
+        'account-settings',
+        'cdn',
+        'cloud',
+        'dns',
+        'hosting',
+        'storage',
+        'streaming',
+        'server-protection',
+        'Web security',
+        'web-security',
+        'Reseller Support',
+        'reseller-support',
+    ],
+});
 
 export const config: ScullyConfig = {
     projectRoot: './src',
     projectName: 'product-documentation',
     outDir: './dist/static',
+    defaultPostRenderers: [baseHrefRewrite],
     puppeteerLaunchOptions:
         process.env.BUILD_ENV === 'develop'
             ? undefined
---
+++ scully/plugins/move-content-files/move-content-files.ts
@@ -0,0 +1,49 @@
+import * as path from 'path';
+import fs from 'fs-extra';
+import { registerPlugin, HandledRoute, scullyConfig, log, green } from '@scullyio/scully';
+
+type MoveContentFilesPluginOptions = {
+    root: string;
+    categories: Array<string>;
+};
+
+const defaultOptions: Required<MoveContentFilesPluginOptions> = {
+    root: 'docs',
+    categories: [
+        'account-settings',
+        'cdn',
+        'cloud',
+        'dns',
+        'hosting',
+        'storage',
+        'streaming',
+        'server-protection',
+        'Web security',
+        'web-security',
+        'Reseller Support',
+        'reseller-support',
+    ],
+};
+
+export const moveContentFiles = 'moveContentFiles';
+
+async function moveContent(routes: Array<HandledRoute>): Promise<void> {
+    const { outDir } = scullyConfig;
+    const root = path.join(outDir, defaultOptions.root);
+
+    if (!fs.existsSync(root)) {
+        fs.mkdirSync(root);
+    }
+
+    defaultOptions.categories.forEach((category) => {
+        const categoryPath = path.join(outDir, category);
+        if (fs.existsSync(categoryPath)) {
+            fs.moveSync(categoryPath, path.join(root, category), { overwrite: true });
+        }
+    });
+
+    log(green('MoveContentFiles finished successfully'));
+}
+
+const validator = async (): Promise<any> => [];
+registerPlugin('allDone', moveContentFiles, moveContent, validator);
