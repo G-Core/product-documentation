fd510ccd0eab46a8ddcecaf1e277b571861debfa
Maria Yurlava <maria.yurlova@gcore.com>
2023-11-03 15:41:59 +0100

    WEB-4888 Fix

--- lint-markdown.sh
+++ lint-markdown.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-# Выполнить линтинг Markdown-файлов в указанной директории
 markdownlint './documentation'
 
-# Проверить код возврата линтера
 if [ $? -ne 0 ]; then
   echo "The linting of Markdown files ended with errors."
-  exit 1  # Завершить сборку с ошибкой
+  exit 1
 fi
