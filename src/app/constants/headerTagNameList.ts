144c589117322334ad50adc7e4536b1e3b2e41f7
Galia Iakusheva <galina.iakusheva@gcore.com>
2024-01-10 14:13:55 +0100

    feat: [WEB-5140] change design & add functionality for toc

---
+++ src/app/constants/headerTagNameList.ts
@@ -0,0 +1,1 @@
+export const headerTagNameList = ['H1', 'H2', 'H3', 'H4', 'H5', 'H6'];
--- src/app/constants/index.ts
+++ src/app/constants/index.ts
@@ -1,2 +1,3 @@
 export * from './common';
 export * from './categories-config';
+export { headerTagNameList } from './headerTagNameList';
