72c5a1dd41f3c1b387fd638503f7a966c6f222b5
Galia Iakusheva <galina.iakusheva@gcore.com>
2024-03-20 16:12:03 +0100

    fix: [WEB-6214] image popup

--- src/app/constants/common.ts
+++ src/app/constants/common.ts
@@ -1,3 +1,4 @@
 export const DOCS_GITHUB_REPO = 'https://github.com/G-Core/product-documentation/edit/main/documentation';
-export const HEADER_HEIGHT = 72;
+export const DISCLAIMER_HEIGHT = 58;
+export const HEADER_HEIGHT = 72 + DISCLAIMER_HEIGHT;
 export const METADATA_FILE_TITLE = 'metadata';
